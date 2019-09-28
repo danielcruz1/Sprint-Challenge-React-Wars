b6df95354bc5293cc53105d9486b31d3c2eef6bb
staccats <47672588+staccats@users.noreply.github.com>
2019-09-27 21:58:46 -0600

    Complete

---
+++ starwars/src/components/StarWarsPlayers.js
@@ -0,0 +1,42 @@
+import React from "react";
+import styled from 'styled-components';
+
+
+
+const PlayerStyle = styled.div`
+width: 30%;	
+background-color: black;
+margin: 20px;	
+color: white;
+border: 5px solid #BD0000;
+border-radius: 25px;
+`;
+
+const PlayerInfo = styled.ul`
+list-style-type: none;
+padding: 0;
+padding-left: 40px;
+color: #10DF00;
+`;
+
+const InfoH1 = styled.li`
+font-size: 1.8rem;
+`
+
+function StarWarsPlayers (props) {
+    return (
+        <PlayerStyle>
+            <PlayerInfo>
+                <li><InfoH1>{props.char.name}</InfoH1></li>
+                <li>Birth Year: {props.char.birth_year}</li>
+                <li>Gender: {props.char.gender}</li>
+                <li>Height:{props.char.height}</li>
+                <li>Mass: {props.char.mass}</li>
+                <li>Eye Color: {props.char.eye_color}</li>
+                
+            </PlayerInfo>
+        </PlayerStyle> 
+    );
+}
+
+export default StarWarsPlayers; 
